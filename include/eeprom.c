15ecd5dd5c44211dd536435d637c7d36d4d6fd33
Andrey Bykanov <shodan@xn--h1aeegel.net>
2015-05-19 07:31:09 +0300

    Добавление новой накачки для версии 4.01+

--- include/eeprom.c
+++ include/eeprom.c
@@ -67,8 +67,14 @@ void eeprom_apply_settings(void)
 	// -------------------------------------------------------------------
 	if(eeprom_read(Geiger_voltage_address) !=Settings.Geiger_voltage) 
  	{
-		ADCData.DAC_voltage_raw=((Settings.Geiger_voltage*1000)/30/11);
-		ADCData.DAC_voltage_raw=(ADCData.DAC_voltage_raw*1000)/ADCData.Calibration_bit_voltage;
+	// Расчет напряжения компаратора
+#ifdef version_401
+		ADCData.DAC_voltage_raw=((Settings.Geiger_voltage*1000)/10/34); // напряжение датчика/Ктансформации/коэффицент резистивного делителя
+#else
+		ADCData.DAC_voltage_raw=((Settings.Geiger_voltage*1000)/30/11); // напряжение датчика/Ктансформации/коэффицент резистивного делителя
+#endif
+		ADCData.DAC_voltage_raw=(ADCData.DAC_voltage_raw*1000)/ADCData.Calibration_bit_voltage; // коррекция значения по напряжению опоры
+
 		DAC_SetChannel2Data(DAC_Align_12b_R, ADCData.DAC_voltage_raw);   /* Set DAC Channel2 DHR register: DAC_OUT2 = (1.224 * 128) / 256 = 0.612 V */
  	}
 	// -------------------------------------------------------------------
